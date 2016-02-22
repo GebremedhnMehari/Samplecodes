cc76ef2e6ff561c29745156975d159b080669b42
Gebremedhn Mehari <geb.meh@gmail.com>
2016-02-22 00:07:35 -0600

    First commit

---
+++ src/Frame1.java
@@ -0,0 +1,114 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.SystemColor;
+import javax.swing.UIManager;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
+public class Frame1 {
+
+	private JFrame frame;
+	private JTextField textFieldNum1;
+	private JTextField textFieldNum2;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	private JTextField textFieldAns;
+	private JLabel lblNewLabel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frame1 window = new Frame1();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Frame1() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(UIManager.getColor("Button.background"));
+		frame.getContentPane().setFont(new Font("Tahoma", Font.BOLD, 13));
+		frame.getContentPane().setLayout(null);
+		
+		textFieldNum1 = new JTextField();
+		textFieldNum1.setBounds(49, 54, 143, 22);
+		frame.getContentPane().add(textFieldNum1);
+		textFieldNum1.setColumns(10);
+		
+		textFieldNum2 = new JTextField();
+		textFieldNum2.setBounds(242, 54, 143, 22);
+		frame.getContentPane().add(textFieldNum2);
+		textFieldNum2.setColumns(10);
+		
+		btnNewButton = new JButton("ADD");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int num1,num2,ans;
+				try {
+					num1=Integer.parseInt(textFieldNum1.getText());
+					num2=Integer.parseInt(textFieldNum2.getText());
+					ans= num1+num2;
+					textFieldAns.setText(Integer.toString(ans));
+				} catch (Exception e1) {
+					JOptionPane.showMessageDialog(null,"please entervalid number");
+				}
+			}
+		});
+		btnNewButton.setBounds(80, 131, 97, 25);
+		frame.getContentPane().add(btnNewButton);
+		
+		btnNewButton_1 = new JButton("Minus");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int num1,num2,ans;
+				try {
+					num1=Integer.parseInt(textFieldNum1.getText());
+					num2=Integer.parseInt(textFieldNum2.getText());
+					ans= num1-num2;
+					textFieldAns.setText(Integer.toString(ans));
+				} catch (Exception e2) {
+					JOptionPane.showMessageDialog(null,"please entervalid number");
+				}
+			}
+		});
+		btnNewButton_1.setBounds(242, 131, 97, 25);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		textFieldAns = new JTextField();
+		textFieldAns.setBounds(210, 201, 116, 22);
+		frame.getContentPane().add(textFieldAns);
+		textFieldAns.setColumns(10);
+		
+		lblNewLabel = new JLabel("The Answer is");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		lblNewLabel.setBounds(80, 204, 112, 16);
+		frame.getContentPane().add(lblNewLabel);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
